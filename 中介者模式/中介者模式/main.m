af397394fb52386903b917862a923f479d2dfe72
liwei02 <weil218@163.com>
2019-01-09 16:37:45 +0800

    中介者模式

---
+++ 中介者模式/中介者模式/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  中介者模式
+//
+//  Created by weil on 2019/1/9.
+//  Copyright © 2019 allyoga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
